9612cb5df946c302a96899d2859505808001bb85
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-02-01 05:04:52 +0000

    Reactive lighting: Hold max brightness for a few ticks before fading out

--- backlighting/purple_reactive.h
+++ backlighting/purple_reactive.h
@@ -2,7 +2,14 @@
 
 void backlight_effect_user(void) {
   for (int i = 0; i < BACKLIGHT_LED_COUNT; i++) {
-    uint16_t hit_time = g_key_hit[i] * 20;
+    uint16_t hit_time = g_key_hit[i];
+    if (hit_time < 3) {
+      hit_time = 0;
+    } else {
+      hit_time = hit_time - 3;
+    }
+
+    hit_time *= 20;
     if (hit_time > 255) hit_time = 255;
 
     uint8_t brightness = 255 - hit_time;
