5a9b4a13b2c1c70ea29a56af5cc33b7826be290d
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-02-01 04:54:58 +0000

    purple_reactive.h should be indented with 2 spaces
    Since I had written this file directly in the wilba_tech sources before,
    I hadn't inherited the .editorconfig

--- backlighting/purple_reactive.h
+++ backlighting/purple_reactive.h
@@ -1,11 +1,13 @@
-void backlight_effect_user(void) {
-    for (int i = 0; i < BACKLIGHT_LED_COUNT; i++) {
-        uint16_t hit_time = g_key_hit[i] * 12;
-        if (hit_time > 255) hit_time = 255;
+#pragma once
 
-        uint8_t brightness = 255 - hit_time;
-        HSV hsv = { .h = 206, .s = 193, .v = brightness };
-        RGB rgb = hsv_to_rgb(hsv);
-        backlight_set_color(i, rgb.r, rgb.g, rgb.b);
-    }
+void backlight_effect_user(void) {
+  for (int i = 0; i < BACKLIGHT_LED_COUNT; i++) {
+    uint16_t hit_time = g_key_hit[i] * 12;
+    if (hit_time > 255) hit_time = 255;
+
+    uint8_t brightness = 255 - hit_time;
+    HSV hsv = { .h = 206, .s = 193, .v = brightness };
+    RGB rgb = hsv_to_rgb(hsv);
+    backlight_set_color(i, rgb.r, rgb.g, rgb.b);
+  }
 }
